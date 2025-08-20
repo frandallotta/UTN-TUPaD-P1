ccba4ba3f729a12bc58f68b820bf2a77719f21bd
frandallotta <franco.gasparin.dallotta@gmail.com>
2025-08-20 15:50:14 -0300

    Create TPn1-Ej04.py

---
+++ 01 - Estructuras Secuenciales/TPn1-Ej04.py
@@ -0,0 +1,5 @@
+radio = float(input("Ingrese el radio de un circulo: "))
+area = (3.14)*radio**2
+perimetro = 2*(3.14)*radio
+
+print (f"Con el radio {radio} ingresado, obtenemos {area} de area y {perimetro} de perimetro")
