6c0072307b69a1a7b6c4078a5e050fafe411ae07
frandallotta <franco.gasparin.dallotta@gmail.com>
2025-08-20 15:50:32 -0300

    Create TPn1-Ej10.py

---
+++ 01 - Estructuras Secuenciales/TPn1-Ej10.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese un numero: "))
+numero3 = int(input("Ingrese un numero: "))
+
+promedio = (numero1 + numero2 + numero3) / 3
+
+print (f"El promedio es: {promedio}")
