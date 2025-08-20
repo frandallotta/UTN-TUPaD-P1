43666ebe0e93615922ee5d1aff169c07bc12889f
frandallotta <franco.gasparin.dallotta@gmail.com>
2025-08-20 15:50:23 -0300

    Create TPn1-Ej06.py

---
+++ 01 - Estructuras Secuenciales/TPn1-Ej06.py
@@ -0,0 +1,23 @@
+numero = int(input("Ingrese un numero: "))
+
+rt1 = numero * 1
+rt2 = numero * 2
+rt3 = numero * 3
+rt4 = numero * 4
+rt5 = numero * 5
+rt6 = numero * 6
+rt7 = numero * 7
+rt8 = numero * 8
+rt9 = numero * 8
+rt10 = numero * 10
+
+print (f"1 x {numero} = {rt1}")
+print (f"2 x {numero} = {rt2}")
+print (f"3 x {numero} = {rt3}")
+print (f"4 x {numero} = {rt4}")
+print (f"5 x {numero} = {rt5}")
+print (f"6 x {numero} = {rt6}")
+print (f"7 x {numero} = {rt7}")
+print (f"8 x {numero} = {rt8}")
+print (f"9 x {numero} = {rt9}")
+print (f"10 x {numero} = {rt10}")
