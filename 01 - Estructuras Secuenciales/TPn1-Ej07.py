0451292fb750926a44e90f39ecf46e9e388480a2
frandallotta <franco.gasparin.dallotta@gmail.com>
2025-08-20 15:50:25 -0300

    Create TPn1-Ej07.py

---
+++ 01 - Estructuras Secuenciales/TPn1-Ej07.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Ingrese un numero distinto a cero: "))
+numero2 = int(input("Ingrese un numero distinto a cero: "))
+
+suma = numero1 + numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+resta = numero1 - numero2
+
+print (f"Sumados: {suma}")
+print (f"Multiplicados: {multiplicacion}")
+print (f"Divididos: {division}")
+print (f"Restados: {resta}")
