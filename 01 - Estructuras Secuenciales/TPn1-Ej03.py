e95e872640d28b7884c6aaf0e682fdc5139ce92f
frandallotta <franco.gasparin.dallotta@gmail.com>
2025-08-20 15:50:10 -0300

    Create TPn1-Ej03.py

---
+++ 01 - Estructuras Secuenciales/TPn1-Ej03.py
@@ -0,0 +1,7 @@
+nombre = input ("Ingrese su nombre: ")
+apellido = input ("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+edad = int(edad)
+residencia = input("Ingrese su lugar de residencia: ")
+
+print(f"Soy {nombre} {apellido}, tengo {edad} anios y vivo en {residencia}")
